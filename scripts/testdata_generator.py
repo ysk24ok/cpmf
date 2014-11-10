7f43504cc04dbb2122fc8297f01ec0d19d9c61a4
Yusuke NISHIOKA <maximilianthermidor765@gmail.com>
2014-11-10 21:46:26 +0900

    Fix testdata_generator.py and regenerate testdata

--- scripts/testdata_generator.py
+++ scripts/testdata_generator.py
@@ -1,17 +1,21 @@
-#!/usr/bin/env puthon
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import sys
 import random
 
 """
-usage: python3 testdata_generator.py 1000 1000 10000 1..5 ../input/testdata
+usage example: python3 testdata_generator.py 1000 1000 10000 1..5 ../input/testdata
 arg1: the number of users
 arg2: the number of items
 arg3: the number of ratings
-arg4: the range of ratings (lower bound and upper bound should be specified)
-arg4: path of the output file
+arg4: the range of ratings (minimum and maximum rating should be specified)
+arg5: path of the output file
 """
+
+def get_msg(user_id, item_id, rating):
+    return str(user_id) + " " + str(item_id) + " " + str(rating) + "\n"
+
 if __name__ == "__main__":
     num_users = int(sys.argv[1])
     if num_users <= 0:
@@ -26,8 +30,8 @@ if __name__ == "__main__":
         sys.exit("the number of ratings should be greater than 0")
 
     rating_scope = list( map(int, sys.argv[4].split("..")) )
-    lower_bound = rating_scope[0]
-    upper_bound = rating_scope[1]
+    minimum_rating = rating_scope[0]
+    maximum_rating = rating_scope[1]
 
     testdata_path = sys.argv[5]
 
@@ -35,16 +39,22 @@ if __name__ == "__main__":
     print("Now processing...")
     existing = []
     f = open(testdata_path, "w")
-    for i in range(0, num_ratings):
+
+    # the case of the maximum numbers of users and items
+    f.writelines( get_msg(random.randint(1, num_users), num_items, random.randint(minimum_rating, maximum_rating)) );
+    f.writelines( get_msg(num_users, random.randint(1, num_items), random.randint(minimum_rating, maximum_rating)) );
+
+    for i in range(0, num_ratings - 2):
+        rand_rating = random.randint(minimum_rating, maximum_rating)
+
         # avoid existing user-item combinations
         while True:
-            rand_user = random.randint(0, num_users)
-            rand_item = random.randint(0, num_items)
-            combination = (rand_user, rand_item)
+            rand_user_id = random.randint(1, num_users)
+            rand_item_id = random.randint(1, num_items)
+            combination = (rand_user_id, rand_item_id)
             if combination not in existing:
                 existing.append(combination)
                 break
-        rand_rating = random.randint(lower_bound, upper_bound)
-        f.writelines([str(rand_user) + " ", str(rand_item) + " ", str(rand_rating)+"\n"])
+        f.writelines( get_msg(rand_user_id, rand_item_id, rand_rating) )
     f.close()
     print("Finished.")
